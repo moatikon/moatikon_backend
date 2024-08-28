4da16597aee531055a97f1083d211e6161052b14
ljyo2o9 <yggosxoljy@gmail.com>
2024-08-28 21:45:07 +0900

    del

--- src/common/get-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { UserEntity } from 'src/user/user.entity';
-
-export const GetUser = createParamDecorator(
-  (data, context: ExecutionContext): UserEntity => {
-    const req = context.switchToHttp().getRequest();
-    return req.user;
-  },
-);
