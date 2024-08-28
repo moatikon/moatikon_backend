8c07b2443cb2745fe3622918193f38f0f91a5dda
ljyo2o9 <yggosxoljy@gmail.com>
2024-08-28 20:22:27 +0900

    update :: CommonModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { UserModule } from "./user/user.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeORMConfig } from "./configs/type-orm.config";
 import { TokenUtilModule } from "./util/token/token-util.module";
+import { CommonModule } from "./common/common.module";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule, TokenUtilModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    UserModule,
+    TokenUtilModule,
+    CommonModule,
+  ],
   controllers: [],
   providers: [],
 })
