1e72229a0a3231d830dac55b6ca40b270907991b
ljyo2o9 <yggosxoljy@gmail.com>
2024-08-29 10:45:35 +0900

    update :: 공백 제거

--- src/common/jwt-refresh.strategy.ts
+++ src/common/jwt-refresh.strategy.ts
@@ -10,19 +10,19 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, "refresh") {
   constructor(
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>
-  ) {    
+  ) {
     super({
       jwtFromRequest: (req) => req.headers["re-token"],
       secretOrKey: jwtReSecret,
     });
   }
 
-  async validate(payload): Promise<UserEntity> {    
+  async validate(payload): Promise<UserEntity> {
     const { userid } = payload;
     const user: UserEntity = await this.userRepository.findOneBy({ userid });
 
-    if(!user) throw new NotFoundException("존재하지 않는 유저입니다.");
-    
+    if (!user) throw new NotFoundException("존재하지 않는 유저입니다.");
+
     return user;
   }
 }
