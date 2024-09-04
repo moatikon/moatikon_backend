95086c50fb8a618e0663db1df364742b742dbdca
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-04 21:58:55 +0900

    update :: exception filter 적용

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { BaseExceptionFilter } from './exception/base-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new BaseExceptionFilter());
   const port = 3000;
 
   await app.listen(port, () => {
-    Logger.log(`Server Started at ${port}port`)
+    Logger.log(`Server Started at ${port}port`);
   });
 }
 bootstrap();
