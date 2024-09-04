fda48ef686c4672c5b3fa854b09bf899b61c2589
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-04 20:22:54 +0900

    add :: user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -7,6 +8,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: ".env",
     }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
