f2d2d23fa42a59a96633eff1b766311628f1ac80
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-04 16:57:29 +0900

    ERR :: config .env적용

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 import { Logger } from "@nestjs/common";
-import { serverPort } from "./configs/configs";
 import { BaseExceptionFilter } from "./exception/base-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = serverPort; // 3000
+  const port = 3000; // 3000
 
   app.useGlobalFilters(new BaseExceptionFilter());
   await app.listen(port);
--- src/util/email/email-util.module.ts
+++ src/util/email/email-util.module.ts
@@ -1,10 +1,33 @@
 import { Module } from "@nestjs/common";
 import { EmailUtilService } from "./email-util-service";
 import { MailerModule } from "@nestjs-modules/mailer";
-import { emailConfig } from "src/configs/email.config";
+import { ConfigService } from "@nestjs/config";
+import { HandlebarsAdapter } from "@nestjs-modules/mailer/dist/adapters/handlebars.adapter";
 
 @Module({
-  imports: [MailerModule.forRoot(emailConfig)],
+  imports: [MailerModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => ({
+      transport: {
+        host: "smtp.gmail.com",
+        port: 587,
+        auth: {
+          user: config.get<string>("EMAIL_USER"),
+          pass: config.get<string>("EMAIL_PASSWORD"),
+        },
+      },
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: "src/util/email/templates/",
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  })],
   providers: [EmailUtilService],
   exports: [EmailUtilService],
 })
