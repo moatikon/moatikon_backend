dab77836dd48e66bd6ec2702dea833b0be1cde64
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-09 09:26:56 +0900

    update :: s3 module / service

--- src/util/s3/s3.service.ts
+++ src/util/s3/s3.service.ts
@@ -1,48 +1,58 @@
-import { DeleteObjectsCommand, PutObjectAclCommand, S3Client } from '@aws-sdk/client-s3';
-import { Injectable } from '@nestjs/common';
+import {
+  DeleteObjectsCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class S3Service {
   s3Client: S3Client;
-  constructor() {
+  constructor(private config: ConfigService) {
     this.s3Client = new S3Client({
-      region: process.env.S3_REGION,
+      region: this.config.get<string>('S3_REGION'),
       credentials: {
-        accessKeyId: process.env.S3_USER_ACCESS,
-        secretAccessKey: process.env.S3_USER_SECRET_ACCESS,
+        accessKeyId: this.config.get<string>('S3_USER_ACCESS'),
+        secretAccessKey: this.config.get<string>('S3_USER_SECRET_ACCESS'),
       },
     });
   }
 
   // Encoding
   #base64Encodeing(originalname: string): string {
-    const buffer = Buffer.from(originalname + uuid(), "utf-8");
-    const encoded = buffer.toString("base64url");
+    const buffer = Buffer.from(originalname + uuid(), 'utf-8');
+    const encoded = buffer.toString('base64url');
 
     return encoded;
   }
 
-  async imageUpload(image: Express.Multer.File) {
-    const filename = this.#base64Encodeing(image.originalname);
-    const ext: string = image.mimetype.split("/")[1];
+  async imageUpload(image: Express.Multer.File): Promise<string> {
+    const filename = this.#base64Encodeing(image.filename);
+    const ext: string = image.mimetype.split('/')[1];
 
-    const commend: PutObjectAclCommand = new PutObjectAclCommand({
+    const commend = new PutObjectCommand({
+      Bucket: this.config.get<string>('S3_BUCKET'),
       Key: `${filename}.${ext}`,
-      Bucket: process.env.S3_BUCKET,
-      ACL: 'public-read-write',
+      Body: image.buffer,
+      ACL: "public-read-write",
+      ContentType: image.mimetype,
     });
-
-    this.s3Client.send(commend);
-    return `https://s3.${process.env.S3_REGION}.amazonaws.com/${process.env.S3_BUCKET}/${filename}.${ext}`;
+  
+    const uploadFileS3 = await this.s3Client.send(commend);
+    if(uploadFileS3.$metadata.httpStatusCode != 200){
+      throw new BadRequestException("S3 File Upload Fail")
+    }
+    return `https://s3.${this.config.get<string>('S3_REGION')}.amazonaws.com/${this.config.get<string>('S3_BUCKET')}/${filename}.${ext}`;
   }
 
   async imageDeleteToS3(image: string): Promise<void> {
-    const tikonImageKey = image.split("/")[4];
+    const tikonImageKey = image.split('/')[4];
 
     const commend = new DeleteObjectsCommand({
-      Bucket: process.env.S3_BUCKET,
+      Bucket: this.config.get<string>('S3_BUCKET'),
       Delete: { Objects: [{ Key: tikonImageKey }] },
     });
 
