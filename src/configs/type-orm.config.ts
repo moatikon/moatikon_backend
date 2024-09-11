726b5840ad0a0803c33f91942e1d2dbdfd7fc801
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-11 09:31:55 +0900

    update :: docker-compose, configs

--- src/configs/type-orm.config.ts
+++ src/configs/type-orm.config.ts
@@ -3,14 +3,14 @@ import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 
 export const typeORMConfig: TypeOrmModuleAsyncOptions = {
   inject: [ConfigService],
-  useFactory: (config: ConfigService) => ({
+  useFactory: () => ({
     type: 'mysql',
-    host: config.get<string>("DB_HOST"),
-    port: config.get<number>("DB_PORT"),
-    username: config.get<string>("DB_USER"),
-    password: config.get<string>("DB_PW"),
-    database: config.get<string>("DB_DB"),
+    host: process.env.DB_HOST, //config.get<string>("DB_HOST"),
+    port: Number(process.env.DB_PORT), //config.get<number>("DB_PORT"),
+    username: process.env.DB_USER, //config.get<string>("DB_USER"),
+    password: process.env.DB_PW, //config.get<string>("DB_PW"),
+    database: process.env.DB_DB, //config.get<string>("DB_DB"),
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: config.get<boolean>("DB_SYNCHRONIZE"),
+    synchronize: Boolean(process.env.DB_SYNCHRONIZE), //config.get<boolean>("DB_SYNCHRONIZE"),
   }),
 };
