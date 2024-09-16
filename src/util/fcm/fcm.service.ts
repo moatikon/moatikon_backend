6ee3223fc03d08fc6358eef1de73318f89a3992e
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-16 16:37:11 +0900

    add :: fcm module / service

---
+++ src/util/fcm/fcm.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common/decorators/core/injectable.decorator';
+import * as admin from 'firebase-admin';
+import { Message } from 'firebase-admin/lib/messaging/messaging-api';
+
+@Injectable()
+export class FcmService {
+  constructor() {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FB_PROJECT_ID,
+        privateKey: process.env.FB_PRIVATE_KEY,
+        clientEmail: process.env.FB_CLIENT_EMAIL,
+      }),
+    });
+  }
+
+  async fcm(token: string, title: string, message: string):Promise<string> {
+    const payload: Message = {
+      token: token,
+      notification: {
+        title: title,
+        body: message,
+      },
+      data: {
+        body: message,
+      },
+    };
+    const result: string = await admin
+      .messaging()
+      .send(payload)
+      .then((response) => response)
+
+      .catch((error) => error.code);
+    return result;
+  }
+}
